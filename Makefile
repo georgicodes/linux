c50b5204fcb29b6bae789082e82e08812a7fb767
Georgi K <georgiknox@gmail.com>
2014-12-24 11:57:05 -0500

    more

--- Makefile
+++ Makefile
@@ -1,19 +1,14 @@
-ifeq ($(KERNELRELEASE),)  
+ifneq ($(KERNELRELEASE),)
+# kbuild part of makefile
+obj-m  := hi.o
 
-KERNELDIR ?= /lib/modules/$(shell uname -r)/build 
-echo $(shell uname -r)
-PWD := $(shell pwd)  
+else
+# normal makefile
+    KDIR ?= /lib/modules/`uname -r`/build
 
-.PHONY: build clean  
-
-build:
-	$(MAKE) -C $(KERNELDIR) M=$(PWD) modules  
-
-clean:
-	rm -rf *.o *~ core .depend .*.cmd *.ko *.mod.c 
-else  
-
-$(info Building with KERNELRELEASE = ${KERNELRELEASE}) 
-obj-m :=    hi.o  
-
+default:
+$(MAKE) -C $(KDIR) M=$$PWD
 endif
+clean:
+	rm -rf *.o *~ core .depend .*.cmd *.ko *.mod.c .*.flags
+	make -C $(KDIR) M=$(PWD) clean
--- hi.c
+++ hi.c
@@ -7,20 +7,18 @@
 
 static int hi(void)
 {
-     printk(KERN_INFO "hi module being loaded.\n");
-     printk(KERN_INFO "User space process is '%s'\n", current->comm);
-     printk(KERN_INFO "User space PID is  %i\n", current->pid);
+     printk(KERN_DEBUG "Hello World!");
      return 0;       // to show a successful load 
 }  
 
 static void bye(void) 
 {
-     printk(KERN_INFO "hi module being unloaded.\n"); 
+     printk(KERN_DEBUG "hi module being unloaded.\n"); 
 }  
 
 module_init(hi);     // what's called upon loading 
 module_exit(bye);    // what's called upon unloading  
 
-MODULE_AUTHOR("Robert P. J. Day"); 
+MODULE_AUTHOR("Georgi K"); 
 MODULE_LICENSE("Dual BSD/GPL"); 
 MODULE_DESCRIPTION("You have to start somewhere.");

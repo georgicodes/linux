6f69a236a0c3b3fb4c5b493be2843568208405c4
Georgi K <georgiknox@gmail.com>
2015-03-29 17:42:50 -0400

    fixed bug

--- char_device/hello_char_extended.c
+++ char_device/hello_char_extended.c
@@ -6,48 +6,24 @@
 #include <linux/fs.h>
  
 static dev_t mydev; /* Global variable for the device number */
-static char output[] = "Be a Linux unicorn!\n";
-struct cdev my_cdev;
 
-ssize_t
-my_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
-{
-    printk(KERN_INFO "In chardrv read routine.\n");
-    printk(KERN_INFO "Count field is %lu.\n", count);
-    printk(KERN_INFO "Offset is %lu.\n", *f_pos);
-
-    if (output[*f_pos] == '\0') {
-        printk(KERN_INFO "End of string, returning zero.\n");
-        return 0;
-    }
-
-    copy_to_user(buf, &output[*f_pos], 1);
-    *f_pos += 1;
-    return 1;  // returned a single character
-}
-
-struct file_operations my_fops = {
-    .owner = THIS_MODULE,
-    .read = my_read,
-};
+static int dev_open(struct inode *, struct file *);
+static int dev_rls(struct inode *, struct file *);
+static ssize_t dev_read(struct file *, char *, size_t, loff_t *);
+static ssize_t dev_write(struct file *, char *, size_t, loff_t *);
 
 static int __init driver_entry(void)
 {
     printk(KERN_INFO "HelloChar: registered");
-
+    
     alloc_chrdev_region(&mydev, 0, 1, "HelloChar");
-
-    cdev_init(&my_cdev, &my_fops);
-    my_cdev.owner = THIS_MODULE;
-    cdev_add(&my_cdev, mydev, 1);
-
+    
     printk(KERN_INFO "HelloChar: <Major, Minor>: <%d, %d>\n", MAJOR(mydev), MINOR(mydev));
     return 0;
 }
  
 static void __exit driver_exit(void)
 {
-    cdev_del(&my_cdev);
     unregister_chrdev_region(mydev, 1);
     printk(KERN_INFO "HelloChar: unregistered");
 }
